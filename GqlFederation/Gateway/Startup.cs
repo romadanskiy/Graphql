9ab0a214869c278bf26f76db81e761c5c001a11b
Артём Сагадеев <sagadeev.artem2001@gmail.com>
2021-12-16 20:18:44 +0300

    project added

---
+++ GqlFederation/Gateway/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using StackExchange.Redis;
+
+namespace Gateway
+{
+    public class Startup
+    {
+        public const string Products = "products";
+        public const string Categories = "categories";
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHttpClient(Products, c => c.BaseAddress = new Uri("http://localhost:5051/graphql"));
+            services.AddHttpClient(Categories, c => c.BaseAddress = new Uri("http://localhost:5052/graphql"));
+            services.AddSingleton(ConnectionMultiplexer.Connect("localhost:7000"));
+
+            services
+                .AddGraphQLServer()
+                .AddQueryType(d => d.Name("Query"))
+                .AddRemoteSchemasFromRedis("Demo", sp => sp.GetRequiredService<ConnectionMultiplexer>());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGraphQL();
+            });
+        }
+    }
+}
